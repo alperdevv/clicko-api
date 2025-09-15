afd8b6fa7b495650f31e4d3ca71c34aa24875508
Alper <68697041+alperdevv@users.noreply.github.com>
2025-09-15 12:40:38 +0200

    Vue Router updated + Users components

--- src/resources/js/router/router.js
+++ src/resources/js/router/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Users from '../components/Users.vue';
 import Login from '../components/Login.vue';
-import UsersList from '../components/Userslist.vue';
+import UsersList from '../components/Users/Userslist.vue';
+import UserEdit from '../components/Users/UserEdit.vue';
+import UserCreate from '../components/Users/UserCreate.vue';
+import UserShow from '../components/Users/UserShow.vue';
 
 const routes = [
     { path: '/users', component: Users },
     { path: '/login', component: Login },
-    { path: '/userslist', component: UsersList }
+    { path: '/userslist', component: UsersList },
+    { path: '/users/:id', component: UserShow },
+    { path: '/users/:id/edit', component: UserEdit },
+    { path: '/users/create', component: UserCreate },
 
 ];
 
