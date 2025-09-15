817387a2001e11142e85d30a7e28d48c046803d0
Alper <68697041+alperdevv@users.noreply.github.com>
2025-09-15 21:31:38 +0200

    Changes to Seeder and README

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        # Genera 20 usuarios aleatorios
         User::factory()->count(20)->create();
+
+        # Genera un usuario base
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@test.com',
+            'password' => Hash::make('123456'),
+        ]);
     }
 }
